bf96e82bdc844ba5c7198893a48f3009c52c51b4
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-04-11 02:55:40 +0530

    covid 19

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 var express = require("express");
 var app = express();
+var request = require("request");
+
+
+//var corona = require("coronavirus-tracker-cli");
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
     res.render("home");
+
+});
+
+app.get("/country", function(req, res) {
+    request("https://corona-stats.online/?format=json", function(err, response, body) {
+
+        if (!err && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            console.log(data);
+            res.render("country", { data: data });
+
+        }
+
+
+
+    });
+
 });
 
 app.listen(3000, function(req, res) {
